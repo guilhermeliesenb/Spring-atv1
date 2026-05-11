5b6385f3c56477fa07e37e065caf4017f3eddc72
guilhermeliesenb <guilherme_liesenb@estudante.sesisenai.org.br>
2026-05-11 08:19:05 -0300

    Initial commit

---
+++ src/main/java/com/example/Spring_atv1/SpringAtv1Application.java
@@ -0,0 +1,13 @@
+package com.example.Spring_atv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAtv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAtv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_atv1/SpringAtv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_atv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAtv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
